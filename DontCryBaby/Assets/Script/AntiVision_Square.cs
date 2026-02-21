7246ad782a9667db89eb5da83378239ee1e441fc
AngeMouquet <114695187+AngeMouquet@users.noreply.github.com>
2026-02-21 12:37:25 +0100

    [FEAT] Add Collisions on wall and anti vision squares

--- DontCryBaby/Assets/Script/AntiVision_Square.cs
+++ DontCryBaby/Assets/Script/AntiVision_Square.cs
@@ -2,19 +2,43 @@ using UnityEngine;
 
 public class AntiVision_Square : MonoBehaviour
 {
+
+    private SpriteRenderer sr;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //get the player object reference here
+        // GameObject BabySitter = GameObject.Find("BabySitter"); //get the player object reference here
+        // if (BabySitter == null) {
+        //     Debug.LogError("Player object not found! Please make sure there is a GameObject named 'BabySitter' in the scene.");
+        // }
+
+        sr = GetComponent<SpriteRenderer>();
+        if (sr == null) {
+            Debug.LogError("SpriteRenderer component not found on AntiVision_Square! Please make sure this script is attached to a GameObject with a SpriteRenderer.");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (1 == 0) {
-            gameObject.SetActive(false);
-        } else {
-            gameObject.SetActive(true);
+        // if () {
+        //     gameObject.SetActive(false);
+        // } else {
+        //     gameObject.SetActive(true);
+        // }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Trigger entered by: " + other.gameObject.name);
+        if (other.gameObject.name == "BabySitter") {
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 0f); // Set opacity to 50% when player enters the trigger
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.name == "BabySitter") {
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 1f); // Reset opacity when player exits the trigger
         }
     }
 }
