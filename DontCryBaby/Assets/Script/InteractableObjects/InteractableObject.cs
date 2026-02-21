ec6a1ce8178ec2ce5fcc40cbd1d869ffc12b5166
alexandreboucardepitech <91665069+alexandreboucardepitech@users.noreply.github.com>
2026-02-21 12:42:14 +0100

    [FEAT] | can interact with objects (but nothing displayed, just prints)

--- DontCryBaby/Assets/Script/InteractableObjects/InteractableObject.cs
+++ DontCryBaby/Assets/Script/InteractableObjects/InteractableObject.cs
@@ -6,22 +6,51 @@ public abstract class InteractableObject : MonoBehaviour
     public string objectName;
     public string interactionText;
     public int clicksRequired = 1;
-    public float cooldown = 0.5f;
+    public float actionCooldown = 0.5f;
+    public float eventCooldown = 0.5f;
+
+    public bool canInteract = true;
 
     protected int currentClicks = 0;
 
     public void Interact()
     {
+        Debug.Log("start interact");
+        if (canInteract == false)
+            return;
+
         currentClicks++;
 
         if (currentClicks >= clicksRequired)
         {
+            Debug.Log("interaction finished");
             CompleteInteraction();
             currentClicks = 0;
+            StartCoroutine(EventCooldownRoutine());
+        }
+        else
+        {
+            Debug.Log("interaction +1");
+            StartCoroutine(ActionCooldownRoutine());
         }
     }
 
-    protected void CompleteInteraction(){
+    private System.Collections.IEnumerator EventCooldownRoutine()
+    {
+        canInteract = false;
+        yield return new WaitForSeconds(eventCooldown);
+        canInteract = true;
+    }
+
+    private System.Collections.IEnumerator ActionCooldownRoutine()
+    {
+        canInteract = false;
+        yield return new WaitForSeconds(actionCooldown);
+        canInteract = true;
+    }
+
+    protected void CompleteInteraction()
+    {
         Debug.Log(interactionText);
     }
 }
--- DontCryBaby/Assets/Script/PlayerController.cs
+++ DontCryBaby/Assets/Script/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+    public float interactRange = 1.5f;
 
     private Rigidbody2D rb;
     private Vector2 movement;
@@ -18,10 +19,54 @@ public class PlayerController : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical") * 2;
 
         movement = movement.normalized;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("key E");
+            TryInteract();
+        }
     }
 
     void FixedUpdate()
     {
         rb.linearVelocity = movement * speed;
+    }
+
+    void TryInteract()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, interactRange);
+
+        InteractableObject closest = null;
+        float minDistance = Mathf.Infinity;
+
+        Debug.Log("try interact");
+        foreach (Collider2D hit in hits)
+        {
+            if (hit.gameObject == gameObject)
+                continue; // éviter de collide avec le babysitter
+            Debug.Log("for each");
+            Debug.Log(hit);
+            InteractableObject interactable = hit.GetComponent<InteractableObject>();
+
+            Debug.Log(interactable);
+            Debug.Log(interactable.canInteract);
+            if (interactable != null && interactable.canInteract)
+            {
+                float distance = Vector2.Distance(transform.position, hit.transform.position);
+
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closest = interactable;
+                }
+            }
+        }
+
+        Debug.Log("finish for each");
+        if (closest != null)
+        {
+            Debug.Log("call interact");
+            closest.Interact();
+        }
     }
 }
